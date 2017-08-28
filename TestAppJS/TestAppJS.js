c42ea577a441d142b171390fed2b0a7d1bb91625
mikhail <mikhail.ronzhin@crystalnix.com>
2017-08-28 17:12:46 +0600

    TestApp JS and JS/Electron

---
+++ TestAppJS/TestAppJS.js
@@ -0,0 +1,3 @@
+
+var objShell = new ActiveXObject("shell.application");
+objShell.ShellExecute("C:\\Program Files (x86)\\Crystalnix\\Update\\1.3.99.0\\GoogleUpdate.exe", "/machine /ua /installsource ondemand", "", "open", 1);
